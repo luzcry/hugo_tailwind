cf2ee98f6063d22a7d9f7fbff07ced2759dab13b
luzcry <mcrodriguezt618@gmail.com>
2021-02-16 18:20:25 -0500

    adding purgecss to the app

--- themes/testinghugo/assets/css/postcss.config.js
+++ themes/testinghugo/assets/css/postcss.config.js
@@ -1,13 +1,15 @@
-const themeDir = __dirname + '/../../';
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [ './hugo_stats.json' ],
+  defaultExtractor: (content) => {
+      let els = JSON.parse(content).htmlElements;
+      return els.tags.concat(els.classes, els.ids);
+  }
+});
 
 module.exports = {
-    plugins: [
-        require('postcss-import')({
-            path: [themeDir]
-            }),
-        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-        require('autoprefixer')({
-            path: [themeDir]
-        }),
-    ]
-}
+  plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      purgecss
+  ]
+};
--- themes/testinghugo/assets/css/tailwind.config.js
+++ themes/testinghugo/assets/css/tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  purge: {
+    enabled: true,
+    preserveHtmlElements: false,
+    layers: ['components', 'utilities'],
+    content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+    css: ['style.css']
+  },
   theme: {
     extend: {}
   },
